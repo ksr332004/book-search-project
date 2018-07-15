ef210848935efa092cca5cc56dc1683064e1d2b5
Seran <41044894+kimsr2004@users.noreply.github.com>
2018-07-16 04:43:12 +0900

    Delete db_DML.sql

--- app-server/bin/main/db_DML.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `user` (`user_id`, `email`, `name`, `password`, `registration_date`) VALUES
-	(1, 'test@test.com', 'test', '123', NOW()),
-	(2, 'admin@test.com', 'admin', '123', NOW()),
-	(3, 'seran@test.com', 'seran', '123', NOW());
-	
