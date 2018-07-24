2e4a5089b94aa10acf5cdb698e24673ec94e926f
Seran <ksr332004@gmail.com>
2018-07-24 20:32:22 +0900

    server: 북마크 부분 수정

---
+++ app-server/src/main/java/com/seran/repository/predicate/BookmarkSearchPredicate.java
@@ -0,0 +1,25 @@
+package com.seran.repository.predicate;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.seran.entity.QBookmark;
+
+
+public class BookmarkSearchPredicate {
+
+    public  static Predicate searchBookmarkByUserIdAndTarget(Integer userId, String target, String query) {
+        QBookmark qBookmark = QBookmark.bookmark;
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(qBookmark.userId.eq(userId));
+
+        if (target.equals("title")) {
+            builder.and(qBookmark.title.like("%" + query + "%"));
+        } else if (target.equals("contents")) {
+            builder.and(qBookmark.contents.like("%" + query + "%"));
+        }
+
+        return builder;
+    }
+
+}
